1b9f5289b012894a5d06666fc33d8af5e3f8d612
Andres Escobar <1520@holbertonschool.com>
2020-02-06 22:26:29 +0000

    0x01. C - Variables, if, else, while TASKS

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,30 +10,35 @@ int main(void)
 	int j;
 	int k;
 	int l;
+	int m = -1;
+	int n;
 
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			for (k = 48; k < 58; k++)
-			{
-			for (l = 48; l < 58; l++)
-			{
-				if (i + j < k + l)
-				{
-				putchar(i);
-				putchar(j);
-				putchar(' ');
-				putchar(k);
-				putchar(l);
-				if (i != 57 || j != 56 || k != 57 || l != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-				}
-			}
-			}
+		n = -1;
+		m++;
+		for (k = 48; k < 58; k++)
+		{
+		for (l = 48; l < 58; l++)
+		{
+		n++;
+		if (n > m)
+		{
+		putchar(i);
+		putchar(j);
+		putchar(' ');
+		putchar(k);
+		putchar(l);
+		if (i != 57 || j != 56 || k != 57 || l != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+		}
+		}
 		}
 	}
 	putchar('\n');
