8bdf1092d252e6c4ad71702a5be08a8ed5120313
Andres Escobar <1520@holbertonschool.com>
2020-02-11 20:06:02 +0000

    0x02. C - Functions, nested loops TASKS

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,9 +1,27 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - 50 de fibonacci.
  * Return: 0
  */
 
 int main(void)
 {
+	unsigned long int n, m, o, p;
+
+	m = 1;
+	o = 1;
+	n = 0;
+	for (; m <= 4000000;)
+	{
+		p = m + o;
+		m = o;
+		o = p;
+		if(m % 2 == 0)
+		{
+			n = n + m;
+		}
+	}
+	printf ("%lu\n", n);
+	return (0);
+}
