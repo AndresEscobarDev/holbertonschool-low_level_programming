33d007dbad6926f6f4ab318281205e9646211394
Andres Escobar <1520@holbertonschool.com>
2020-02-11 15:33:27 +0000

    0x02. C - Functions, nested loops TASKS

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -7,18 +7,21 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		if (n < 0)
+		while (n < 98)
 		{
-			for (; n < 0; n++)
-			{
-				_putchar();
-			{
-		}
-		else
-		{
-
+			printf("%i, ", n);
+			n++;
 		}
 	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%i, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
