7b577a5b59ebac6e2632572ad6ab3d7a1d61c0e6
Andres Escobar <1520@holbertonschool.com>
2020-02-13 18:23:16 +0000

    0x04. C - More functions, more nested loops TASKS

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size >= 0)
+	if (size > 0)
 	{
 	for (i = 0; i < size; i++)
 	{
@@ -18,7 +18,7 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-	if (size == 0)
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
