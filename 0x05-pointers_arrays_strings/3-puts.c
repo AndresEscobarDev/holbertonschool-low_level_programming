15e044bdc43a3d3368da3ddd6d095429fcf8bf55
Andres Escobar <1520@holbertonschool.com>
2020-02-18 06:43:46 +0000

    0x05. C - Pointers, arrays and strings TASKS

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _puts - print string.
+ * @str: string.
+ * Return: Nothing.
  */
 void _puts(char *str)
 {
