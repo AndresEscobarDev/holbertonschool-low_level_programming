beb2c174d116925e3dbed82a89b233cefdb27fe7
Andres Escobar <1520@holbertonschool.com>
2020-02-18 16:09:19 +0000

    0x05. C - Pointers, arrays and strings TASKS

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; a[i] && i < n; i++)
 	{
 		if (i == n - 1)
-			printf("%i", a[i]);
+			printf("%d", a[i]);
 		else
-			printf("%i, ", a[i]);
+			printf("%d, ", a[i]);
 	}
 	printf("\n");
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -22,14 +22,14 @@ char *_strcpy(char *dest, char *src)
 }
 /*
 strncpy(char *dest, const char *src, size_t n)
-{
-	size_t i;
+           {
+               size_t i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+               for (i = 0; i < n && src[i] != '\0'; i++)
+                   dest[i] = src[i];
+               for ( ; i < n; i++)
+                   dest[i] = '\0';
 
-	return dest;
-}
+               return dest;
+           }
 */
