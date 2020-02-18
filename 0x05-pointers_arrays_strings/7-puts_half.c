1f7990157a119b4a58c1c9fd1416f1becec806ee
Andres Escobar <1520@holbertonschool.com>
2020-02-18 15:06:10 +0000

    0x05. C - Pointers, arrays and strings TASKS

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ void puts_half(char *str)
 
 	while (str[i])
 		i++;
-	if(i % 2 == 0)
+	if (i % 2 == 0)
 		i = i / 2;
 	else
-		i = (i - 1) / 2;
+		i = ((i + 1) / 2);
 	while (str[i])
 	{
 		_putchar(str[i]);
