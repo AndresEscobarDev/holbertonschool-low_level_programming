669035d2504179fe4ae9236841dafc534cba93fa
Andres Escobar <1520@holbertonschool.com>
2020-02-20 20:03:38 +0000

    0x06. C - More pointers, arrays and strings TASKS

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,7 +27,7 @@ char *cap_string(char *s)
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] -= 32;
 		}
-		if (s[i] == '}')
+		if (s[i] == '}' || s[i] == '\t')
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] -= 32;
