bea89527e4ae2c7c332d11ae661af573fb490023
Andres Escobar <1520@holbertonschool.com>
2020-02-21 02:48:47 +0000

    0x06. C - More pointers, arrays and strings TASKS

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+ * voltear - swap.
+ *@r: Strng or char.
+ *@k: lengt.
+ * Return: r.
+ */
+char *voltear(char *r, int k)
+{
+	char n;
+	int p;
+
+	k--;
+	for (p = 0; p < k; p++)
+	{
+		n = r[k];
+		r[k] = r[p];
+		r[p] = n;
+		k--;
+	}
+	return (r);
+}
+/**
  * infinite_add - Sum giants numbers.
  *@n1:numero 1.
  *@n2:numero 2.
@@ -10,83 +31,44 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int p, i = 0, j = 0, k, s, l = 0;
-	char n;
+	int i = 0, j = 0, k, s, l = 0, ic, jc;
 
 	while (n1[i])
 		i++;
 	while (n2[j])
 		j++;
-	i--;
-	j--;
-	for (k = 0; i >= 0 && j >= 0; k++, i--, j--)
+	for (i--, j--, k = 0; i >= 0 || j >= 0; k++, i--, j--)
 	{
-		if ((n1[i] - '0' + n2[j] - '0' + l) > 9)
+		if (i >= 0)
+			ic = n1[i] - '0';
+		else
+			ic = 0;
+		if (j >= 0)
+			jc = n2[j] - '0';
+		else
+			jc = 0;
+		if ((ic + jc + l) > 9)
 		{
-			s = (n1[i] - '0' + n2[j] - '0' + l) % 10;
-			l = (n1[i] - '0' + n2[j] - '0' + l) / 10;
+			s = (ic + jc + l) % 10;
+			l = (ic + jc + l) / 10;
 		}
 		else
 		{
-			s = n1[i] - '0' + n2[j] - '0' + l;
+			s = ic + jc + l;
 			l = 0;
 		}
 		r[k] = s + '0';
 	}
-		if (i >= 0)
-		{
-			for (; i >= 0; k++, i--)
-			{
-				if ((n1[i] - '0' + l) > 9)
-				{
-					s = (n1[i] - '0' + l) % 10;
-					l = (n1[i] - '0' + l) / 10;
-				}
-				else
-				{
-					s = n1[i] - '0' + l;
-					l = 0;
-				}
-				r[k] = s + '0';
-			}
-		}
-		else if (j >= 0)
-		{
-			for (; j >= 0; k++, j--)
-			{
-				if ((n2[j] - '0' + l) > 9)
-				{
-					s = (n2[j] - '0' + l) % 10;
-					l = (n2[j] - '0' + l) / 10;
-				}
-				else
-				{
-					s = n2[j] - '0' + l;
-					l = 0;
-				}
-				r[k] = s + '0';
-			}
-		}
-		if (l > 0)
-		{
-			r[k] = l + '0';
-			k++;
-		}
-		r[k] = '\0';
-		if (k >= size_r)
-		{
-			return (0);
-		}
-		else
-		{
-			k--;
-			for (p = 0; p < k; p++)
-			{
-				n = r[k];
-				r[k] = r[p];
-				r[p] = n;
-				k--;
-			}
-			return (r);
-		}
+	if (l > 0)
+	{
+		r[k] = l + '0';
+		k++;
+	}
+	r[k] = '\0';
+	if (k >= size_r)
+	{
+		return (0);
+	}
+	else
+		return (voltear(r, k--));
 }
