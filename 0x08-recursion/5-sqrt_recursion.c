7ab2f1b2978c368e0b80ccb13f989de547c8acd0
Andres <1520@holbertonschool.com>
2020-02-26 18:28:54 +0000

    0x08. C - Recursion TASKS

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,37 @@
 #include "holberton.h"
 /**
+ * pow_number - function that returns the square root of a number.
+ * @n: Number.
+ * @m: Number.
+ * Return: m.
+ */
+int pow_number(int n, int m)
+{
+	if (m * m != n && m > 0)
+	{
+		m = pow_number(n, m - 1);
+	}
+	else if (m * m == n)
+	{
+		return (m);
+	}
+	return (m);
+}
+/**
  * _sqrt_recursion - function that returns the natural square root of a number.
  * @n: Number.
  * Return: n.
  */
 int _sqrt_recursion(int n)
 {
-	if(n == 0)
-	return (0);
+	if (n == 0)
+		return (0);
 	if (n < 0)
 		return (-1);
 	if (n > 0)
 	{
-		if (
-		c = n - 1;
-		_sqrt_recursion()
-
+		if (pow_number(n, n) * pow_number(n, n) == n)
+			return (pow_number(n, n));
 	}
+	return (-1);
 }
