069207a659675fb4b74930f2035aa27706b8a310
Andres Escobar <felipeescobar15@gmail.com>
2020-03-03 16:44:46 -0500

    0x0B. C - malloc, free TASKS

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,8 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return ('\0');
 	a = malloc(height * sizeof(int));
+	if (a == NULL)
+		return ('\0');
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(width * sizeof(int));
