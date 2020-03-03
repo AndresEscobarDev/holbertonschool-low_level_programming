3883b9aea2595f87d66fe4d550a34af7da38f403
Andres Escobar <felipeescobar15@gmail.com>
2020-03-03 14:56:10 -0500

    0x0B. C - malloc, free TASKS

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,11 +20,10 @@ char *_strdup(char *str)
 			return ('\0');
 		*s = '\0';
 		return (s);
-
 	}
 	while (str[n])
 		n++;
-	s = malloc((n) * sizeof(char));
+	s = malloc((n + 1) * sizeof(char));
 	if (s == NULL)
 		return ('\0');
 	for (i = 0; i <= n; i++)
