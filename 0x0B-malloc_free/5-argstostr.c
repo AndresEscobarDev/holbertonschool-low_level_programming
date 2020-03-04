b836ccf2e486b18f0507e36116d5f4686c78f988
Andres Escobar <felipeescobar15@gmail.com>
2020-03-03 19:16:12 -0500

    0x0B. C - malloc, free TASKS

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,30 +1,32 @@
 #include <stdlib.h>
 /**
- * 
- * 
+ * argstostr - function that concatenates all the arguments of your program.
+ * @ac: Number of args.
+ * @av: Args.
+ * Return: Null or s.
  */
 char *argstostr(int ac, char **av)
 {
-    char *s;
-    int i, j, n = 0;
+	char *s;
+	int i, j, n = 0;
 
-    if (ac == 0 || av == 0)
-        return(0);
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; i++)
-            n++;
-    }
-    s = malloc((ac + n + 1) * sizeof(char));
-    if (s == 0)
-        return (0);
-    for (i = 0, n = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; i++, n++)
-            s[n] = av[i][j];
-        s[n] = '\n';
-        n++;
-    }
-    s[n] = 0;
-    return (s);
+	if (ac == 0 || av == 0)
+		return (0);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			n++;
+	}
+	s = malloc((ac + n + 1) * sizeof(char));
+	if (s == 0)
+		return (0);
+	for (i = 0, n = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++, n++)
+			s[n] = av[i][j];
+		s[n] = '\n';
+		n++;
+	}
+	s[n] = 0;
+	return (s);
 }
