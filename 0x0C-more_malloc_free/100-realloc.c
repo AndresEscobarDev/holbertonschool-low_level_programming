12474bd2650896ddab95b8bc531392d4777ed31d
Andres Escobar <felipeescobar15@gmail.com>
2020-03-05 14:58:11 -0500

    0x0C. C - More malloc, free TASKS

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - function that reallocates a memory block using malloc and free.
+ * @ptr: Old pointer.
+ * @old_size: Old Size.
+ * @new_size: New Size.
+ * Return: n or NULL.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *n, *s = ptr;
+	unsigned int i = 0;
+
+	if (ptr == 0 && new_size == 0)
+	{
+		free(ptr);
+		return (0);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	n = malloc(new_size);
+	if (n == 0)
+		return (0);
+	while (i < new_size)
+	{
+		n[i] = s[i];
+		i++;
+	}
+	free(ptr);
+	return (n);
+}
