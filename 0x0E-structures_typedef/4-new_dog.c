9dda42a42a494abf1349395383ebac5241d954f9
Andres Felipe Escobar <felipeescobar15@gmail.com>
2020-03-09 17:40:14 -0500

    0x0E. C - Structures, typedef TASKS

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,8 +37,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; name[i]; i++)
 		d->name[i] = name[i];
+	d->name[i] = 0;
 	for (i = 0; owner[i]; i++)
 		d->owner[i] = owner[i];
+	d->owner[i] = 0;
 	d->age = age;
 	return (d);
 }
