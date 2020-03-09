716f508fb2a705759108b5272cbd8dcab4366459
Andres Felipe Escobar <woltfang@DESKTOP-TKNNSA1.localdomain>
2020-03-09 14:50:05 -0500

    0x0E. C - Structures, typedef TASKS

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner : owner of the dog.
+ * Description: information about the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
