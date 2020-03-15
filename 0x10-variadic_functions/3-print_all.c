51a3df2d0628435453ad294d2337f0c19012e488
Andres Felipe Escobar <felipeescobar15@gmail.com>
2020-03-14 21:34:55 -0500

    0x10. C - Variadic functions TASKS

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,45 +2,78 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * fo_char - print char.
+ * @c: character.
+ * Return: Nothing.
+ */
+void fo_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+/**
+ * fo_int - print int.
+ * @n: integer.
+ * Return: Nothing.
+ */
+void fo_int(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+/**
+ * fo_float - print float.
+ * @n: character.
+ * Return: Nothing.
+ */
+void fo_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+/**
+ * fo_string - print string.
+ * @s: string.
+ * Return: Nothing.
+ */
+void fo_string(va_list n)
+{
+	char *s = va_arg(n, char *);
+	if (!s || !*s)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
+}
+/**
  * print_all - function that prints anything.
  * @format: format to print.
  * Return: Nothing.
  */
 void print_all(const char * const format, ...)
 {
+	int i = 0, j = 0;
 	va_list vl;
-	unsigned int i = 0;
-	char *s;
+	fo_t fos[] = {
+        {'c', fo_char},
+        {'i', fo_int},
+        {'f', fo_float},
+        {'s', fo_string},
+        {0, 0}
+    };
 
 	va_start(vl, format);
-	while (format && format[i])
+	while (format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (fos[j].fo)
 		{
-		case 'c':
-			printf("%c", va_arg(vl, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(vl, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(vl, double));
-			break;
-		case 's':
-			s = va_arg(vl, char *);
-			if (!s || !*s)
+			if (fos[j].fo == format[i])
 			{
-				printf("(nil)");
-				break;
+				fos[j].f(vl);
+				if (format[i + 1])
+					printf(", ");
 			}
-			printf("%s", s);
-			break;
-		default:
-			break;
+			j++;
 		}
-		if (i + 1 != strlen(format) && (format[i] == 'c' ||
-		format[i] == 's' || format[i] == 'i' || format[i] == 'f'))
-			printf(", ");
 		i++;
 	}
 	printf("\n");
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,18 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+typedef struct fo
+{
+    char fo;
+    void (*f)(va_list);
+} fo_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void fo_string(va_list);
+void fo_float(va_list);
+void fo_int(va_list);
+void fo_char(va_list);
 
 #endif /* VARIADIC_FUNCTIONS_H */
