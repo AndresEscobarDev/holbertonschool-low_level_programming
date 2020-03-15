9fea3449b97aaf36d6d98df197114dc011a39c78
Andres Felipe Escobar <felipeescobar15@gmail.com>
2020-03-14 19:35:21 -0500

    0x10. C - Variadic functions TASKS

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,47 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * fo_char - print char.
- * @c: character.
- * Return: Nothing.
- */
-void fo_char(char c)
-{
-	printf("%c", c);
-}
-/**
- * fo_int - print int.
- * @n: integer.
- * Return: Nothing.
- */
-void fo_int(int n)
-{
-	printf("%d", n);
-}
-/**
- * fo_float - print float.
- * @n: character.
- * Return: Nothing.
- */
-void fo_float(float n)
-{
-	printf("%f", n);
-}
-/**
- * fo_string - print string.
- * @s: string.
- * Return: Nothing.
- */
-void fo_string(char *s)
-{
-	if (!s || !*s)
-	{
-		printf("(nil)");
-		return;
-	}
-		printf("%s", s);
-}
-/**
  * print_all - function that prints anything.
  * @format: format to print.
  * Return: Nothing.
@@ -51,28 +10,36 @@ void print_all(const char * const format, ...)
 {
 	va_list vl;
 	unsigned int i = 0;
+	char *s;
 
 	va_start(vl, format);
-	while (format[i])
+	while (format && format[i])
 	{
 		switch (format[i])
 		{
 		case 'c':
-			fo_char(va_arg(vl, int));
+			printf("%c", va_arg(vl, int));
 			break;
 		case 'i':
-			fo_int(va_arg(vl, int));
+			printf("%d", va_arg(vl, int));
 			break;
 		case 'f':
-			fo_float(va_arg(vl, double));
+			printf("%f", va_arg(vl, double));
 			break;
 		case 's':
-			fo_string(va_arg(vl, char *));
+			s = va_arg(vl, char *);
+			if (!s || !*s)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", s);
 			break;
 		default:
 			break;
 		}
-		if (i + 1 != strlen(format) && (format[i] == 'c' || 'i' || 'f' || 's'))
+		if (i + 1 != strlen(format) && (format[i] == 'c' ||
+		format[i] == 's' || format[i] == 'i' || format[i] == 'f'))
 			printf(", ");
 		i++;
 	}
