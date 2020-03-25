88a681cd7a52370c2807fe1eb5cad1cd4ba40837
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-03-25 00:21:48 -0500

    Fixing last advance task

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,14 +7,22 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *head2 = head;
+	listint_t *head2 = head, *head1 = head;
 
-	while (head && head2 && head2->next)
+	while (head1 && head2 && head2->next)
 	{
-		head = head->next;
+		head1 = head1->next;
 		head2 = head2->next->next;
-		if (head == head2)
-			return (head);
+		if (head1 == head2)
+		{
+			head1 = head;
+			while (head1 != head2)
+			{
+				head1 = head1->next;
+				head2 = head1->next;
+			}
+			return (head1);
+		}
 	}
 	return (0);
 }
