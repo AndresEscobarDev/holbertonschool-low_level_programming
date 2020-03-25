bed672b35e87178fb673b5926a2a9a987f251ff7
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-03-24 23:00:00 -0500

    Second advance task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint_safe - function that prints a listint_t linked list.
+ * @head: Head of the listint.
+ * Return: Leng of list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+    int i = 0;
+
+    while (head)
+    {
+        i++;
+        printf("[%p] %d\n", (void *)head, head->n);
+        if (head - head->next > 0)
+            head = head->next;
+        else
+        {
+            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+            break;
+        }
+    }
+    return (i);
+}
