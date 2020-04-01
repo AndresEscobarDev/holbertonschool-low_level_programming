a560edebaf441fdbd65a572c77421276b4f800b8
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-03-31 20:06:22 -0500

    1 Task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: File name.
+ * @text_content: string to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0, j;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content && text_content[i])
+		i++;
+	if (i > 0)
+	{
+		j = write(fd, text_content, i);
+		if (j == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
