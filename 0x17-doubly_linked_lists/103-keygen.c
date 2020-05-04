f5ac9960befecae46b52ad31b26f52af2f683688
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-04 00:32:41 -0500

    Last advance task

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,42 +3,43 @@
 #include <string.h>
 /**
  * main - program that print a keygen for the crackme5
- * 
- * 
+ * @argc: len of string
+ * @argv: user
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    int len = strlen(argv[1]), i, j;
-    char codigo[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-    char password[6];
+	int len = strlen(argv[1]), i, j;
+	char c[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-    (void)argc;
-    j = (len ^ 59) & 63;
-    password[0] = codigo[j];
-    for (i = 0, j = 0; i < len; i++)
-        j += argv[1][i];
-    j = (j ^ 79) & 63;
-    password[1] = codigo[j];
-    for (i = 0, j = 1; i < len; i++)
-        j *= argv[1][i];
-    j = (j ^ 85) & 63;
-    password[2] = codigo[j];
-    for (i = 0, j = 0; i < len; i++)
-    {
-        if (j < argv[1][i])
-            j = argv[1][i];
-    }
-    srand(j ^ 14);
-    password[3] = codigo[rand() & 63];
-    for (i = 0, j = 0; i < len; i++)
-        j += argv[1][i] * argv[1][i];
-    j = (j ^ 239) & 63;
-    password[4] = codigo[j];
-    for (i = 0, j = 0; i < argv[1][0]; i++)
-        j = rand();
-    j = (j ^ 229) & 63;
-    password[5] = codigo[j];
-    password[6] = 0;
-    printf("%s", password);
-    return(0);
+	char password[6];
+	(void)argc;
+	j = (len ^ 59) & 63;
+	password[0] = c[j];
+	for (i = 0, j = 0; i < len; i++)
+		j += argv[1][i];
+	j = (j ^ 79) & 63;
+	password[1] = c[j];
+	for (i = 0, j = 1; i < len; i++)
+		j *= argv[1][i];
+	j = (j ^ 85) & 63;
+	password[2] = c[j];
+	for (i = 0, j = 0; i < len; i++)
+	{
+		if (j < argv[1][i])
+			j = argv[1][i];
+	}
+	srand(j ^ 14);
+	password[3] = c[rand() & 63];
+	for (i = 0, j = 0; i < len; i++)
+		j += argv[1][i] * argv[1][i];
+	j = (j ^ 239) & 63;
+	password[4] = c[j];
+	for (i = 0, j = 0; i < argv[1][0]; i++)
+		j = rand();
+	j = (j ^ 229) & 63;
+	password[5] = c[j];
+	password[6] = 0;
+	printf("%s", password);
+	return (0);
 }
